822b6ba37710868f7c62e23a64ee7c11ecc1b9c8
Mastantonto <lazaro.esart18@zaragoza.salesianos.edu>
2025-09-16 11:48:41 +0200

    Add files via upload

---
+++ suma.py
@@ -0,0 +1,9 @@
+def Suma():
+    #cadena=raw_input("dime tu nombre: ")
+    #print("bienvenido " + cadena)
+    num1=input("Dime un numero: ")
+    num2=input("Dime otro numero: ")
+    print(str(num1)+" + " +str(num2)+ " = "+str(num1+num2))
+
+
+Suma()
