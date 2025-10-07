a2bf19e7005ab6f051145ab563433599fddb772c
Mastantonto <lazaro.esart18@zaragoza.salesianos.edu>
2025-10-07 11:52:00 +0200

    Add files via upload

---
+++ palabra_mas_larga.py
@@ -0,0 +1,7 @@
+def palabra_mas_larga():
+    palabra1=input("Dime una palabra: ")
+    palabra2=input("Dime otra palabra: ")
+    for cont in range(1,palabra1,+1):
+        if(palabra1==is len(palabra2)):
+            print(
+        
