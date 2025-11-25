ca28e9d5a3493ff387c85e3a8986194227afe50b
Mastantonto <lazaro.esart18@zaragoza.salesianos.edu>
2025-11-25 11:16:49 +0100

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,17 @@
+def programa_1() :
+    max_num = float(input("Introduce un número: "))
+    sum_nums = max_num
+    
+    for i in range(9):
+    
+        
+        num = float(input("Introduce otro número "))
+        
+        sum_nums =sum_nums+ (num)
+        if num > max_num:
+            
+            max_num = num
+    print("The largest number is:",max_num )
+    print("The average is:", sum_nums/10)
+    
+programa_1()
